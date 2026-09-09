579f89c59d88aa76bab817c821d0bda6ecc9239b
unknown <jonamk07ts@gmail.com>
2026-09-09 14:03:04 -0600

    clase de condiciones

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+alert ("bienvenidos a mi juego");
+
+let numeroSecreto=7
+
+let numeroUsuario=prompt("ingresa un numero del 1 al 10");
+
+console.log(numeroUsuario);
+
+// inicia condicion 
+
+if (numeroSecreto == numeroUsuario) {
+    alert ("acertaste");
+}
+alert ("sigue intentando");
+
+*/
+
+let calif = prompt("¿cual es la calificacion?");
+
+
+
+console.log(calif);
+
+// inicia condicion
+
+if (calif >= 6) {
+    alert("aprobaste");
+}
+alert ("reprobado");
