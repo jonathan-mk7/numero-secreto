dbc37014244e3a1eb24ed2a21ac7c9be6feb8417
unknown <jonamk07ts@gmail.com>
2026-09-10 13:19:34 -0600

    ejercicios de condicion}

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*
+
 alert ("bienvenidos a mi juego");
 
 let numeroSecreto=7
@@ -14,17 +14,43 @@ if (numeroSecreto == numeroUsuario) {
 }
 alert ("sigue intentando");
 
-*/
+
+
+
+/*
+
+//condicion de calificaciones
+
 
 let calif = prompt("¿cual es la calificacion?");
-
 
 
 console.log(calif);
 
 // inicia condicion
 
-if (calif >= 6) {
+if (calif >= 6) {    
+    if (calif >= 9) {
+        alert("excelente");
+
+    }
     alert("aprobaste");
 }
 alert ("reprobado");
+
+
+
+//condicion de edad
+
+let edad = prompt("¿cual es tu edad?");
+
+console.log(edad);
+
+//inicia condicion
+
+if (edad >= 18) {
+    alert("acceso permitido");
+}
+alert ("acceso denegado");
+
+*/
