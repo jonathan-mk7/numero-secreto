e74a9e501c97106561a60e012f40cb9809855874
unknown <jonamk07ts@gmail.com>
2026-09-14 17:00:33 -0600

    añadi carpeta de desafios y termine juego

---
+++ Desafios/main.js
@@ -0,0 +1,44 @@
+/*
+let dia = prompt("¿que dia de la semana es?");
+if (dia==="sabado"|| dia ==="domingo"){
+    alert ("buen fin de semana");
+
+}else{
+    alert("buena semana");
+}
+
+
+practicas
+//condicion de calificaciones
+
+
+let calif = prompt("¿cual es la calificacion?");
+
+
+console.log(calif);
+
+// inicia condicion
+
+if (calif >= 6) {    
+    if (calif >= 9) {
+        alert("excelente");
+
+    }
+    alert("aprobaste");
+}
+alert ("reprobado");
+
+
+//condicion de edad
+
+let edad = prompt("¿cual es tu edad?");
+
+console.log(edad);
+
+//inicia condicion
+
+if (edad >= 18) {
+    alert("acceso permitido");
+}
+alert ("acceso denegado");
+*/
--- app.js
+++ app.js
@@ -1,70 +1,44 @@
 
-/*
+
 
 alert ("bienvenidos a mi juego");  //alerta de bienvenida al juego
 
-let numeroSecreto=8 //declaracion de variable del numero secreto
+let numeroSecreto=Math.floor(Math.random()*10)+1;//declaracion de variable del numero secreto
+let numeroUsuario=0
 
-let numeroUsuario=prompt("ingresa un numero del 1 al 10"); //variable para pedir al usuario un numero y guardarlo en (numeroUsuario)
+let intentos=1       //numero de intentos
+//let palabraVeces="vez" 
+let maximoIntentos=5
 
-console.log(numeroUsuario); // muestra resultados en la consola
+while (numeroSecreto != numeroUsuario) {   // inicia condicion (codigo para realizar comparacion)
+    let numeroUsuario=prompt("ingresa un numero del 1 al 10"); //variable para pedir al usuario un numero y guardarlo en (numeroUsuario)
 
-// inicia condicion (codigo para realizar comparacion)
+    console.log(numeroSecreto); // muestra resultados en la consola
 
-if (numeroSecreto == numeroUsuario) {   // condicion 
-    //la condicion es verdadera
-    alert (`acertaste, el numero secreto es: ${numeroSecreto}`);                // para mostrar en ventana emergente si se cumplio la condicion 
-} else{                                 // condicion ( si no) es falsa la condicion
-alert ("sigue intentando");             // para mostrar en ventana emergente
-}
+    if (numeroSecreto == numeroUsuario) {   // condicion 
 
-*/
+        alert (`acertaste, el numero secreto es: ${numeroSecreto}. lo hiciste en: ${intentos} ${intentos == 1 ? 'Vez' : 'veces'}`);             //operadores ternarios
+                        // para mostrar en ventana emergente si se cumplio la condicion 
+    } else{    // condicion ( si no) es falsa la condicion (se agrega una pista)
+        if (numeroUsuario > numeroSecreto ) {     //condicion si el numero es mayor se da una pista
+            alert ("el numero es menor");    
 
+        } else {
+            alert ("el numero es mayor");    //si es mayor se muestra en ventana emergente
+        }                             
+            //incrementamos el contador cuando no acierta
+            intentos ++
+           //palabraVeces="veces" 
+           if (intentos > maximoIntentos) {
+            alert (`llegaste al numero maximo de: ${intentos} intentos`);
+            break;    //para cerrar el ciclo
 
-let dia = prompt("¿que dia de la semana es?");
-if (dia==="sabado"|| dia ==="domingo"){
-    alert ("buen fin de semana");
-
-}else{
-    alert("buena semana");
-}
-
-
-
-/*
-practicas
-//condicion de calificaciones
-
-
-let calif = prompt("¿cual es la calificacion?");
-
-
-console.log(calif);
-
-// inicia condicion
-
-if (calif >= 6) {    
-    if (calif >= 9) {
-        alert("excelente");
-
+           }
     }
-    alert("aprobaste");
+
 }
-alert ("reprobado");
 
 
 
-//condicion de edad
 
-let edad = prompt("¿cual es tu edad?");
 
-console.log(edad);
-
-//inicia condicion
-
-if (edad >= 18) {
-    alert("acceso permitido");
-}
-alert ("acceso denegado");
-
-*/
